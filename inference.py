133b8e7f667a1d83812109af836a80c6f8dfa772
krg <kaushikgunjkar52@gmail.com>
2026-04-03 01:11:21 +0530

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,6 @@
+# This file redirects the grader to your new server logic
+from server.app import app, main
+
+if __name__ == "__main__":
+    # This calls the same main function we just built
+    main()
